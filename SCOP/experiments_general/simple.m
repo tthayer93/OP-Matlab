3ed7b732389f3b31c74c58be5b2b33f16917018a
Thomas C. Thayer <tthayer@ucmerced.edu>
2021-03-09 22:24:42 -0800

    fixed call to unavailable function

--- SCOP/experiments_general/simple.m
+++ SCOP/experiments_general/simple.m
@@ -1,11 +1,11 @@
 %% Initialize
 clear all;
 close all hidden;
-%rng(2);
+rng(2);
 addpath(genpath('~/Sync/Grad_School/Code'));
 %% Parameters
 n_vertex = 10
-t_max = 2
+t_max = 1.25
 num_time_steps = 10
 %rewards = [[1:n_vertex]', ones(n_vertex, 1)];
 %rewards = [[1:n_vertex]', random('exponential', 1, [n_vertex, 1])];
@@ -32,7 +32,7 @@ rewards(start_vertex, 2) = 0;
 %% Solve with old method
 [op_tour, op_reward, op_cost] = solve_OP(edge_list, rewards(:, 2), t_max, start_vertex, end_vertex);
 [vertex_cluster_rewards, vertex_clusters] = OP_route_to_SCOP_expected_costs(op_tour, edge_list, rewards, cost_dist_type, alpha);
-[states, state_transition_table, initial_state_distribution, absorbing_states] = clustered_SCOP_to_CMDP_multifail_empirical_heuristic2_1(vertex_cluster_rewards, vertex_clusters, t_max, num_time_steps);
+[states, state_transition_table, initial_state_distribution, absorbing_states] = clustered_SCOP_to_CMDP_multifail(vertex_cluster_rewards, vertex_clusters, t_max, num_time_steps);
 tic
 [policy, state_action, rho, old_output] = solve_CMDP(state_transition_table, initial_state_distribution, absorbing_states, target_failure_rate);
 toc
